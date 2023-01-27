78f780447c782d5b6ce28da075dac09da0ed2b37
Jarvis Carpo <jvcarpo@student.apc.edu.ph>
2023-01-27 09:40:40 +0800

    W3 PHP Lessons

---
+++ week8/index.php
@@ -0,0 +1,8 @@
+ <!DOCTYPE html>
+<html>
+<body> <?php
+echo "My first PHP script!";
+echo "<br> WEBPROG Week 8! Go!";
+?> </body>
+</html>
+
---
+++ week8/print.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+print "<h2>HELLO</h2>";
+print "BONJOUR<br>";
+print "KAMUSTA";
+?> 
+
+</body>
+</html>
